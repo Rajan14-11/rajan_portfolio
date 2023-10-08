6a6e731ed4272da0626b86714b95bb93be26723b
Rajan14-11 <rajangoyal125@gmail.com>
2023-10-08 19:06:49 +0530

    added footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import { BsGithub, BsYoutube, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { BsGithub, BsYoutube, BsInstagram, BsLinkedin, BsWhatsapp } from "react-icons/bs";
 import "./Footer.css";
 
 const Footer = () => {
@@ -10,7 +10,8 @@ const Footer = () => {
       <div>
         <Typography variant="h5">About Me</Typography>
         <Typography>
-          Hey, my name is Rajan Goyal. I am a Frontend Developer and learning full stack development.
+          Hey, my name is Rajan Goyal. I am a Frontend Developer and learning
+          full stack development.
         </Typography>
 
         <Link to="/contact" className="footerContactBtn">
@@ -19,16 +20,16 @@ const Footer = () => {
       </div>
       <div>
         <Typography variant="h6">Social Media</Typography>
-        <a href="https://github.com/meabhisingh/" target="black">
+        <a href="https://github.com/rajan14-11/" target="black">
           <BsGithub />
         </a>
-        <a href="https://youtube.com/6packprogrammer/" target="black">
-          <BsYoutube />
+        <a href="https://wa.me/7652932128" target="black">
+          <BsWhatsapp />
         </a>
-        <a href="https://instagram.com/meabhisingh/" target="black">
+        <a href="https://instagram.com/rajangoyal740/" target="black">
           <BsInstagram />
         </a>
-        <a href="https://www.linkedin.com/in/meabhisingh/" target="black">
+        <a href="https://www.linkedin.com/in/rajan-goyal/" target="black">
           <BsLinkedin />
         </a>
       </div>
